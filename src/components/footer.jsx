ac46956989b32359914e412a77d315d19a2e3c58
zlmx <3448585471@qq.com>
2024-10-04 21:17:11 +0800

    update

---
+++ src/components/footer.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { GithubOne, Home, Mail } from "@icon-park/react";
+import CustomLink from "@/components/customLink";
+
+const Footer = () => {
+  // 加载配置
+  const githubName = import.meta.env.VITE_GITHUB_NAME;
+  const homeUrl = import.meta.env.VITE_HOME_URL;
+  const emailUrl = import.meta.env.VITE_EMAIL_URL;
+  const siteIcp = import.meta.env.VITE_SITE_ICP;
+
+  return (
+    <footer id="footer">
+      <div className="social">
+        <CustomLink
+          iconDom={<GithubOne />}
+          to={`https://github.com/${githubName}/`}
+        />
+        <CustomLink iconDom={<Home />} to={homeUrl} />
+        <CustomLink iconDom={<Mail />} to={`mailto:${emailUrl}`} />
+      </div>
+      <div className="text">
+        <p>
+          Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
+          &nbsp;
+          <CustomLink to="https://yunzai-bot.cn/" text="ZLMX" />
+          {siteIcp ? (
+            <React.Fragment>
+              &nbsp;|&nbsp;
+              <CustomLink to="https://beian.miit.gov.cn/" text={siteIcp} />
+            </React.Fragment>
+          ) : null}
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
